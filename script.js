6d2ef6fa882a350b22aa9aabf4313738c245c086
Kofeinisti <jani.sillanpaa@hotmail.com>
2025-02-25 21:32:57 +0200

    Korjattu laskenta

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 document.getElementById("calculate").addEventListener("click", function () {
     let sum =
-        parseInt(document.getElementById("50e").value) * 50 +
-        parseInt(document.getElementById("20e").value) * 20 +
-        parseInt(document.getElementById("10e").value) * 10 +
-        parseInt(document.getElementById("5e").value) * 5 +
-        parseInt(document.getElementById("2e").value) * 2 +
-        parseInt(document.getElementById("1e").value) +
-        parseInt(document.getElementById("50c").value) * 0.5 +
-        parseInt(document.getElementById("20c").value) * 0.2 +
-        parseInt(document.getElementById("10c").value) * 0.1 +
-        parseInt(document.getElementById("5c").value) * 0.05;
+        parseFloat(document.getElementById("50e").value) * 50 +
+        parseFloat(document.getElementById("20e").value) * 20 +
+        parseFloat(document.getElementById("10e").value) * 10 +
+        parseFloat(document.getElementById("5e").value) * 5 +
+        parseFloat(document.getElementById("2e").value) * 2 +
+        parseFloat(document.getElementById("1e").value) * 1 +
+        parseFloat(document.getElementById("50c").value) * 0.5 +
+        parseFloat(document.getElementById("20c").value) * 0.2 +
+        parseFloat(document.getElementById("10c").value) * 0.1 +
+        parseFloat(document.getElementById("5c").value) * 0.05;
 
-    
     document.getElementById("sum").textContent = sum.toFixed(2) + "€";
 });
 
+// Teeman vaihtaja
 document.getElementById("theme-toggle").addEventListener("click", function () {
     document.body.classList.toggle("light-mode");
 });
